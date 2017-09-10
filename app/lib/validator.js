6b41a5fab0315441e6eebeb9ef9e5c12e3a80368
hsinlu <hsinlu@live.com>
2017-09-11 00:12:49 +0800

    完善校验类

--- app/lib/validator.js
+++ app/lib/validator.js
@@ -41,126 +41,126 @@ class Validator {
   }
 
   in (values, errMsg) {
-    if (!validator.isIn(this._value, values)) {
+    if (this._value === undefined || !validator.isIn(this._value, values)) {
       this._addErr(errMsg || `${this._name} 必须是 ${values.join('、')}之一`)
     }
     return this
   }
 
   contains(seed, errMsg) {
-    if (!validator.contains(this._value, seed)) {
+    if (this._value === undefined || !validator.contains(this._value, seed)) {
       this._addErr(errMsg || `${this._name} 必须包含 ${seed}`)
     }
     return this
   }
 
   equals(comparison, errMsg) {
-    if (!validator.equals(this._value, comparison)) {
+    if (this._value === undefined || !validator.equals(this._value, comparison)) {
       this._addErr(errMsg || `${this._name} 必须为 ${comparison}`)
     }
     return this
   }
 
   before(date, errMsg) {
-    if (!validator.isBefore(this._value, date)) {
+    if (this._value === undefined || !validator.isBefore(this._value, date)) {
       this._addErr(errMsg || `${this._name} 必须早于 ${date}`)
     }
     return this
   }
 
   after(date, errMsg) {
-    if (!validator.isAfter(this._value, date)) {
+    if (this._value === undefined || !validator.isAfter(this._value, date)) {
       this._addErr(errMsg || `${this._name} 必须晚于 ${date}`)
     }
     return this
   }
 
   boolean(errMsg) {
-    if (!validator.isBoolean(this._value)) {
+    if (this._value === undefined || !validator.isBoolean(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是布尔值`)
     }
     return this
   }
 
   date(errMsg) {
-    if (!validator.isDate(this._value)) {
+    if (this._value === undefined || !validator.isDate(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是日期`)
     }
     return this
   }
 
   decimal(errMsg) {
-    if (!validator.isDecimal(this._value)) {
+    if (this._value === undefined || !validator.isDecimal(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是小数`)
     }
     return this
   }
 
   float(errMsg) {
-    if (!validator.isFloat(this._value)) {
+    if (this._value === undefined || !validator.isFloat(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是浮点数`)
     }
     return this
   }
 
   int(errMsg) {
-    if (!validator.isInt(this._value)) {
+    if (this._value === undefined || !validator.isInt(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是整数`)
     }
     return this
   }
 
   numerict(errMsg) {
-    if (!validator.isNumeric(this._value)) {
+    if (this._value === undefined || !validator.isNumeric(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是数字`)
     }
     return this
   }
 
   url(errMsg) {
-    if (!validator.isURL(this._value)) {
+    if (this._value === undefined || !validator.isURL(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是URL`)
     }
     return this
   }
 
   uuid(errMsg) {
-    if (!validator.isUUID(this._value)) {
+    if (this._value === undefined || !validator.isUUID(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是UUID`)
     }
     return this
   }
 
   email(errMsg) {
-    if (!validator.isEmail(this._value)) {
+    if (this._value === undefined || !validator.isEmail(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是邮箱`)
     }
     return this
   }
 
   ip(errMsg) {
-    if (!validator.isIP(this._value)) {
+    if (this._value === undefined || !validator.isIP(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是IP地址`)
     }
     return this
   }
 
-  mobile(errMsg) {
-    if (!validator.isMobilePhone(this._value)) {
+  mobile(errMsg, locale = 'zh-CN') {
+    if (this._value === undefined || !validator.isMobilePhone(this._value, locale)) {
       this._addErr(errMsg || `${this._name} 必须是手机号`)
     }
     return this
   }
 
   matches(pattern, errMsg) {
-    if (!validator.matches(this._value, pattern)) {
+    if (this._value === undefined || !validator.matches(this._value, pattern)) {
       this._addErr(errMsg || `${this._name} 无法匹配 ${pattern}`)
     }
     return this
   }
 
   mongoId(errMsg) {
-    if (!validator.isMongoId(this._value)) {
+    if (this._value === undefined || !validator.isMongoId(this._value)) {
       this._addErr(errMsg || `${this._name} 必须是Mongo Id`)
     }
     return this
