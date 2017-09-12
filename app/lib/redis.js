e208d44ce145ae2cf54de8a8d6bb888449c8c7f7
hsinlu <hsinlu@live.com>
2017-09-12 23:53:26 +0800

    修复logger未引入

--- app/lib/redis.js
+++ app/lib/redis.js
@@ -1,6 +1,7 @@
 const redis = require('redis')
 const bluebird = require('bluebird')
 const config = require('../config')
+const logger = require('./logger')
 
 bluebird.promisifyAll(redis.RedisClient.prototype)
 bluebird.promisifyAll(redis.Multi.prototype)
